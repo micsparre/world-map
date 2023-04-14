63e50cee6f2299c6e41253d7857bbef11a66f3fa
Michael Sparre <michaelsparre@berkeley.edu>
2023-04-14 09:41:25 -0700

    got working rotating globe

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MapChart from "./MapChart";
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>My Travel Map</h1>
       <MapChart />
     </div>
   );
--- src/MapChart.tsx
+++ src/MapChart.tsx
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 const geoUrl =
   "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
 
 const MapChart: React.FC = () => {
   const [visitedCountries, setVisitedCountries] = useState<string[]>(localStorage.getItem("visitedCountries") ? JSON.parse(localStorage.getItem("visitedCountries") || "") : []);
+  const [rotation, setRotation] = useState<[number, number, number]>([0, 0, 0]);
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleMouseDown = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    setIsDragging(true);
+    setMouseX(event.clientX);
+    setMouseY(event.clientY);
+  };
+
+  const handleMouseMove = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    if (isDragging) {
+      const deltaX = event.clientX - mouseX;
+      const deltaY = event.clientY - mouseY;
+      setRotation([rotation[0] + deltaX / 2, rotation[1] - deltaY / 2, 0]);
+      setMouseX(event.clientX);
+      setMouseY(event.clientY);
+    }
+  };
 
   const handleCountryClick = (geo: any) => {
     const country = geo.properties.name;
@@ -23,41 +43,49 @@ const MapChart: React.FC = () => {
     localStorage.setItem("visitedCountries", JSON.stringify(visitedCountries));
   }, [visitedCountries]);
 
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
   return (
     <ComposableMap 
-    projection='geoMercator' 
+    projection='geoOrthographic' 
     projectionConfig={{
-      scale: 90,
+      rotate: rotation, // Set the rotation based on state
+      scale: 200,
     }}
     width={900}
-    height={600}
+    height={450}
     style={{ width: "100%", height: "auto" }} 
+    onMouseDown={handleMouseDown}
+    onMouseMove={handleMouseMove}
+    onMouseUp={handleMouseUp}
     >
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo : any) => (
-            <Geography
-              key={geo.rsmKey}
-              geography={geo}
-              onClick={() => handleCountryClick(geo)}
-              style={{
-                default: {
-                  fill: visitedCountries.includes(geo.properties.name) ? "#355C7D" : "#D6D6DA",
-                  outline: "#D6D6DA",
-                },
-                hover: {
-                  fill: "#6C5B7B",
-                  outline: "#2A363B",
-                },
-                pressed: {
-                  fill: "#F67280",
-                  outline: "none",
-                },
-              }}
-            />
-          ))
-        }
-      </Geographies>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo : any) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                onClick={() => handleCountryClick(geo)}
+                style={{
+                  default: {
+                    fill: visitedCountries.includes(geo.properties.name) ? "#355C7D" : "#D6D6DA",
+                    outline: "#D6D6DA",
+                  },
+                  hover: {
+                    fill: "#6C5B7B",
+                    outline: "#2A363B",
+                  },
+                  pressed: {
+                    fill: "#F67280",
+                    outline: "none",
+                  },
+                }}
+              />
+            ))
+          }
+        </Geographies>
     </ComposableMap>
   );
 };
