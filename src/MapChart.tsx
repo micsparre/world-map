899724890fe3e3877e4962f607e8269795f4f1e4
Michael Sparre <micsparre@gmail.com>
2023-11-28 14:46:30 -0800

    added country name displayed at bottom of page when hovering

--- src/MapChart.tsx
+++ src/MapChart.tsx
@@ -20,6 +20,7 @@ const MapChart: React.FC = () => {
   const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
   const [isDragging, setIsDragging] = useState(false);
+  const [tooltipCountry, setTooltipCountry] = useState<any>();
 
   const handleMouseDown = (
     event: React.MouseEvent<SVGSVGElement, MouseEvent>
@@ -81,7 +82,7 @@ const MapChart: React.FC = () => {
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
       >
-        <Sphere id="ocean" fill="#5eabdb" stroke="#D6D6DA" strokeWidth={1} />
+        <Sphere id="ocean" fill="#5eabdb" stroke="#D6D6DA" strokeWidth={0} />
         <Geographies geography={process.env.PUBLIC_URL + "/features.json"}>
           {({ geographies }) =>
             geographies.map((geo: any) => (
@@ -89,20 +90,30 @@ const MapChart: React.FC = () => {
                 key={geo.rsmKey}
                 geography={geo}
                 onClick={() => handleCountryClick(geo)}
+                onMouseEnter={() => {
+                  console.log(geo);
+                  setTooltipCountry(geo);
+                }}
+                onMouseLeave={() => {
+                  setTooltipCountry(undefined);
+                }}
                 style={{
                   default: {
                     fill: visitedCountries.includes(geo.properties.name)
                       ? "#184d08"
                       : "#D6D6DA",
                     outline: "#D6D6DA",
+                    zIndex: 0,
                   },
                   hover: {
                     fill: "#6C5B7B",
                     outline: "#2A363B",
+                    zIndex: 0,
                   },
                   pressed: {
                     fill: "#F67280",
                     outline: "none",
+                    zIndex: 0,
                   },
                 }}
               />
@@ -111,6 +122,9 @@ const MapChart: React.FC = () => {
         </Geographies>
       </ComposableMap>
       <button onClick={handleResetClick}>Reset Rotation</button>
+      {tooltipCountry && (
+        <h2 style={{ color: "white" }}> {tooltipCountry.properties.name} </h2>
+      )}
     </div>
   );
 };
